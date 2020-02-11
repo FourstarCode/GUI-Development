6eded5c775016f6c63d377b06a72929b0fda17e7
Client 0120 <Client 0120@DESKTOP-59ELKUT>
2020-02-11 15:14:01 +0000

    edit

---
+++ src/messageDialogBox/MessageDialogBox.java
@@ -0,0 +1,11 @@
+package messageDialogBox;
+
+import javax.swing.JOptionPane;
+public class MessageDialogBox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"Java GUI Developer","Welcome User",JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
